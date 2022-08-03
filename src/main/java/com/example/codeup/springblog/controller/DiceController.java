34c4f6f5f06f2234952b71ac9c9c18bb990b4af7
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-08-03 14:40:20 -0400

    feat: test test

---
+++ src/main/java/com/example/codeup/springblog/controller/DiceController.java
@@ -0,0 +1,28 @@
+package com.example.codeup.springblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Random;
+
+@Controller
+public class DiceController {
+
+    @GetMapping("/roll-dice")
+     public String diceRoll(){
+
+        return "roll-dice";
+    }
+    @GetMapping("/roll-dice{guess}")
+    public String diceRollResults(@PathVariable int guess, Model vModel){
+
+        Random rn = new Random();
+        int roll = rn.nextInt(6) + 1;
+        vModel.addAttribute("roll",roll);
+        vModel.addAttribute("guess", guess);
+        return "roll-results";
+
+    }
+}
---
+++ src/main/java/com/example/codeup/springblog/repositories/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.codeup.springblog.repositories;
+import com.example.codeup.springblog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
