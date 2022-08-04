279f0a34c928236a1e99260303fb709eb43dcdd1
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-08-04 09:52:35 -0400

    feat test3 git

--- src/main/java/com/example/codeup/springblog/controller/PostController.java
+++ src/main/java/com/example/codeup/springblog/controller/PostController.java
@@ -1,35 +1,72 @@
 package com.example.codeup.springblog.controller;
 
+import com.example.codeup.springblog.model.Post;
+import com.example.codeup.springblog.model.User;
+import com.example.codeup.springblog.repositories.PostRepository;
+import com.example.codeup.springblog.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PostController {
 
+    private PostRepository postDao;
+    private UserRepository userDao;
+    private final EmailService emailService;
+
+    public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService) {
+        this.postDao = postDao;
+        this.userDao = userDao;
+        this.emailService = emailService;
+    }
+
     @GetMapping("/posts")
-    @ResponseBody
-    public String getPosts(){
-        return "viewing all post ";
+    public String getPosts(Model vModel) {
+        List<Post> posts = postDao.findAll();
+        vModel.addAttribute("posts", posts);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String getPost(@PathVariable long id) {
-        return "Viewing post: " + id;
+    public String getPost(@PathVariable long id, Model vModel) {
+        Post post = postDao.findById(id).get();
+        vModel.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
-    public String getCreatePostForm() {
-        return "Form to create post.";
+    public String getCreatePostForm(Model model) {
+        model.addAttribute("newPost", new Post());
+        return "posts/create";
     }
 
-    @PostMapping ("/posts/create")
-    @ResponseBody
-    public String savePost() {
-        return "Saving post.";
+    @PostMapping("/posts/create")
+    public String savePost(@ModelAttribute Post newPost) {
+        User user = userDao.findById(1L).get();
+        newPost.setUser(user);
+        postDao.save(newPost);
+        emailService.prepareAndSend(newPost, "You created a new post!");
+        return "redirect:/posts";
     }
+
+    @GetMapping("/posts/{id}/edit")
+    public String showEditForm(@PathVariable long id, Model model) {
+        Post postToEdit = postDao.findById(id).get();
+        model.addAttribute("postToEdit", postToEdit);
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String submitEditForm(@ModelAttribute Post postToEdit) {
+        User user = userDao.findById(1L).get();
+        postToEdit.setUser(user);
+        postDao.save(postToEdit);
+        return "redirect:/posts";
+    }
+
 }
--- src/main/java/com/example/codeup/springblog/model/Post.java
+++ src/main/java/com/example/codeup/springblog/model/Post.java
@@ -4,11 +4,14 @@ import javax.persistence.*;
 @Entity
 @Table(name = "posts")
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false, columnDefinition = "TEXT")
     private String body;
 
@@ -21,43 +24,45 @@ public class Post {
         this.title = title;
         this.body = body;
     }
+
+    public Post() {
+    }
+
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
     }
 
-    public Post(long id) {
-        this.id = id;
-    }
-
-    public Post() {
-
-    }
-
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getBody() {
         return body;
     }
+
     public void setBody(String body) {
         this.body = body;
+    }
 
-        public User getUser() {
-            return user;
-        }
+    public User getUser() {
+        return user;
+    }
 
-        public void setUser(User user) {
-            this.user = user;
-        }
+    public void setUser(User user) {
+        this.user = user;
     }
 }
+
