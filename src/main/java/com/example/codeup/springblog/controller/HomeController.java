fcf9775dde5900c5d109c5ab2aafbecd1a8b9b68
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-08-25 08:34:58 -0400

    feat: Form Model Binding exercise

--- src/main/java/com/example/codeup/springblog/controller/HomeController.java
+++ src/main/java/com/example/codeup/springblog/controller/HomeController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
+
     public String land() {
-        return "this is the landing page";
+        return "index";
     }
 }
